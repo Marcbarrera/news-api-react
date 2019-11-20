b620c3d12a3f3a96158296e3d5360a728a773968
marcbarrera <barreramarinmarc@gmail.com>
2019-11-20 11:50:51 +0100

    News API done with React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, fragment } from 'react'
+import Header from './components/Header'
+import ListaNoticias from './components/ListaNoticias'
+import Formulario from './components/Formulario'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+ class App extends Component {
+   state= {
+     noticias:[]
+
+   }
+
+  componentDidMount(){
+    this.consultarNoticias();
+  }
+
+   consultarNoticias = async (categoria = 'general') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=cbf15a7cbf944bd58aca002a9a92f34f`;
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias: noticias.articles
+    })
+  }
+
+  render() {
+    return ( 
+      <fragment>
+        <Header
+        titulo='Noticias React API'
+        />
+        <div className="container white contenedor-noticias">
+              <Formulario
+                consultarNoticias={this.consultarNoticias}
+              />
+              
+              <ListaNoticias
+                noticias={this.state.noticias}
+              />
+        </div>
+        
+      </fragment>
+    )
+  }
 }
-
-export default App;
+export default App
